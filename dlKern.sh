f2dfb224e39817cd707fbec1ed9b042ebb95e2f4
Jari Ojanen <jojanen@elisanet.fi>
2017-11-21 21:57:23 +0200

    rename dlKern.sh -> do.sh (only one script that contains all the current ones)

--- dlKern.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-VERSION=4.9.56
-
-cd /mnt
-wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-${VERSION}.tar.xz
-tar xf linux-${VERSION}.tar.xz
-rm linux-${VERSION}.tar.xz
---
+++ do.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+VERSION=4.9.64
+KVERSION=30
+
+case $1 in
+    dk)	
+	echo "download kernel..." 
+	cd /mnt
+	wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-${VERSION}.tar.xz
+	tar xf linux-${VERSION}.tar.xz
+	rm linux-${VERSION}.tar.xz
+	;;
+    bx)
+	echo "build x86 kernel ${VERSION}-${KVERSION}"
+	cd /mnt/linux-${VERSION}
+
+	make nconfig
+	cp .config /home/jari/config_${KVERSION}
+
+	#make-kpkg clean
+	#fakeroot make-kpkg -j3 --initrd --append-to-version=-j${VERSION} --revision=${VERSION} kernel_image kernel_headers
+
+	make clean
+	make -j2 KDEB_PKGVERSION=${KVERSION} LOCALVERSION=.j${KVERSION} bindeb-pkg
+	;;
+    br)
+	echo "build RPI kernel"
+	cd /mnt/linux-${VERSION}
+
+	#make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- multi_v7_defconfig
+
+	make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- nconfig
+	cp .config /home/jari/rpi_${KVERSION}
+    
+	make -j3 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf-
+	;;
+    three)
+	echo "3"
+	;;
+    *)
+	echo "Unknown command: $1"
+	;;
+esac
+
